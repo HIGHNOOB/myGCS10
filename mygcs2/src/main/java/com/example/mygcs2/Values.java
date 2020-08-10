9fda6bb27490813b75f09a7e0ecaf6b0b0cf030b
Sweep <45086666+HIGHNOOB@users.noreply.github.com>
2020-08-10 12:55:19 +0900

    관리를위해 값들분류 예정

---
+++ mygcs2/src/main/java/com/example/mygcs2/Values.java
@@ -0,0 +1,9 @@
+package com.example.mygcs2;
+
+import android.graphics.Color;
+
+public class Values {
+    public static int POLYGON_OUTLINE_WIDTH = 5;
+    public static int POLYGON_OUTLINE_COLOR = Color.BLUE;
+    public static int POLYGON_COLOR = Color.TRANSPARENT;
+}
