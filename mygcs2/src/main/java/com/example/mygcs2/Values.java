0e81f2c36a8795aa64c1b3fb214ee331c0a1f4c2
jw969 <jw96962006@naver.com>
2020-08-13 21:55:32 +0900

    .

--- mygcs2/src/main/java/com/example/mygcs2/Values.java
+++ mygcs2/src/main/java/com/example/mygcs2/Values.java
@@ -3,10 +3,25 @@ package com.example.mygcs2;
 import android.graphics.Color;
 
 public class Values {
+    public static int TMP_DISTANCE =5;
+
+
     public static int POLYGON_OUTLINE_WIDTH = 5;
     public static int POLYGON_OUTLINE_COLOR = Color.BLUE;
     public static int POLYGON_COLOR = Color.TRANSPARENT;
 
     public static int START_POINT_NEAREST = 0;
     public static int START_POINT_LONGEST = 1;
+
+    public static double EARTH = 6378.137;  //radius of the earth in kilometer
+
+    public static int DIAGONAL_TYPE_HORIZONTAL = 0;
+    public static int DIAGONAL_TYPE_VERTICAL = 1;
+    public static int DIAGONAL_TYPE_NW_TO_SE = 2;
+    public static int DIAGONAL_TYPE_NE_TO_SW = 3;
+
+    public static int DIRECTION_NE =0;
+    public static int DIRECTION_NW =1;
+    public static int DIRECTION_SE =2;
+    public static int DIRECTION_SW =3;
 }
